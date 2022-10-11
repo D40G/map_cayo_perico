e4038f74696a32d4995edd9ff0fa27d9be35f00e
RexShack <rexshackuk@gmail.com>
2022-10-11 01:01:29 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'RSG : Cayo Perico Island'
+
+this_is_a_map 'yes'
+
+client_scripts {
+
+    'client/client.lua',
+	'client/peds_config.lua',
+	'client/main_peds.lua',
+
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+    "interiorproxies.meta"
+}
